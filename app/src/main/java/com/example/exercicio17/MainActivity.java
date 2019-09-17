e6e37a1088b91386077c7c3abb3157c9f4a119a8
Guilherme <guilherme-thums@Hotmail.com>
2019-09-17 18:45:34 -0300

    Spinner funcionando
    só falta reGex CEP

--- app/src/main/java/com/example/exercicio17/MainActivity.java
+++ app/src/main/java/com/example/exercicio17/MainActivity.java
@@ -6,15 +6,13 @@ import android.content.Context;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import java.lang.reflect.Array;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,49 +21,43 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        String[] paises = getResources().getStringArray(R.array.array_paises);
-        String[] estados = getResources().getStringArray(R.array.array_estados);
-        String[] cidades = getResources().getStringArray(R.array.array_cidades_goias);
+        final String[] paises = getResources().getStringArray(R.array.array_paises);
+        final String[] estados = getResources().getStringArray(R.array.array_estados);
+        final String[] cidades = getResources().getStringArray(R.array.array_cidades_goias);
 
         final Spinner spinnerPais = (Spinner) findViewById(R.id.pais);
         final Spinner spinnerEstado = (Spinner) findViewById(R.id.estado);
         final Spinner spinnerCidade = (Spinner) findViewById(R.id.cidade);
 
-        ArrayAdapter<String> paisAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, paises);
-        ArrayAdapter<String> estadoAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, estados);
-        ArrayAdapter<String> cidadeAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_list_item_1, cidades);
+        final ArrayAdapter<String> paisAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, paises);
+        final ArrayAdapter<String> estadoAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, estados);
+        final ArrayAdapter<String> cidadeAdapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_dropdown_item, cidades);
 
         spinnerPais.setAdapter(paisAdapter);
-        spinnerEstado.setAdapter(estadoAdapter);
-        spinnerCidade.setAdapter(cidadeAdapter);
 
-        spinnerPais.setOnItemClickListener(new AdapterView.OnItemSelectedListener(){
-
-            public void onItemSelectedListener(AdapterView<?> arg0, View arg1,int position, long arg3){
-                spinnerPais.setSelection(position);
+        spinnerPais.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                if(position == 28){
+                    spinnerEstado.setAdapter(estadoAdapter);
+                }
             }
-
-            public void onNothingSelected(AdapterView<?> arg0){
-
-            }
-
-        });
-
-        spinnerEstado.setOnItemClickListener(new AdapterView.OnItemClickListener(){
-            public void onItemSelectedListener(AdapterView<?> arg0, View arg1, int position, long arg3){
-                spinnerEstado.setSelection(position);
-            }
-
-            public void onNothingSelected(AdapterView<?> arg0){
-
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
             }
         });
 
-
-
-        Spinner textView = findViewById(R.id.pais);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, paises);
-        textView.setAdapter(adapter);
+        spinnerEstado.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int position, long l) {
+                if(position == 8){
+                    spinnerCidade.setAdapter(cidadeAdapter);
+                }
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+            }
+        });
 
     }
 
@@ -86,17 +78,7 @@ public class MainActivity extends AppCompatActivity {
         //EditText cep = findViewById(R.id.cep);
         //int cepNumero = Integer.parseInt(cep.getText().toString());
 
-
-        /*AutoCompleteTextView pais = findViewById(R.id.pais);
-        String paisText = pais.getText().toString();
-
-        AutoCompleteTextView estado = findViewById(R.id.estado);
-        String estadoText = estado.getText().toString();
-
-        AutoCompleteTextView cidade = findViewById(R.id.cidade);
-        String cidadeText = cidade.getText().toString();
-
-
+        /*
         boolean nomeRegex = Pattern.matches("\\d", nomeText);
         boolean sobrenomeRegex = Pattern.matches("\\d", sobrenomeText);
         boolean cepRegex = Pattern.matches();
@@ -107,7 +89,10 @@ public class MainActivity extends AppCompatActivity {
         } else if (TextUtils.isEmpty(sobrenomeText)) {
             Toast toast = Toast.makeText(context, "Campo 'sobrenome' vazio!", duracao);
             toast.show();
-        }
+        } else if (TextUtils.isEmpty(enderecoText)){
+            Toast toast = Toast.makeText(context, "Campo 'endereço' vazio!", duracao);
+            toast.show();
+        } //else if ()
 
 
 
